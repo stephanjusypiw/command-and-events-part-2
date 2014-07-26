983dda9da53fb43b682d6552ed6f126a02336097
Stephan Jusypiw <stephanj@internode.on.net>
2014-07-27 08:36:12 +1000

    Commands and Events - route file edited

---
+++ app/controllers/JobController.php
@@ -0,0 +1,20 @@
+<?php
+
+class JobController extends \BaseController {
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /job
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		return "store";
+	}
+
+
+
+
+}
---
+++ app/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+/**
+ * resource route
+ */
+Route::post('jobs/store', [
+    'as' => 'jobs/store',
+    'uses' => 'JobController@store'
+]);
